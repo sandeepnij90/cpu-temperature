ab66262c2cd21619ee123eef7d67f9d4369c5e9d
Sandeep Nijjar <sandeepnijjar@Sandeeps-MBP.lan>
2021-05-16 12:51:37 +0100

    get ambient temperature and style fixes

--- preload.js
+++ preload.js
@@ -1,8 +1,25 @@
 const si = require('systeminformation');
+const getLocation = require('electron-get-location')
+const weatherAPI = '03592d5eb56a6155391a03f010728a2b'
+
+
+const setCPUTemperature = async () => {
+    const temperature = await si.cpuTemperature();
+    document.getElementById('cpu').innerText = `${temperature.main}°C`;
+}
+
+const setAmbientTemperature = async () => {
+    const {latitude, longitude} = await getLocation()
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weatherAPI}&units=metric`)
+    const { main: { temp } } = await response.json()
+    console.log(temp)
+    document.getElementById('temp').innerText = `${Math.ceil(temp)}°C`
+}
 
 window.addEventListener('DOMContentLoaded', async () => {
-    const temperature = await si.cpuTemperature();
-    const element = document.getElementById('replace');
-    if (element) element.innerText = `The CPU temperature is: ${temperature.main} °C`;   
+    setCPUTemperature()
+    setAmbientTemperature()
 })
+
+
 
